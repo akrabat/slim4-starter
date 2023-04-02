2b9585fd984e3e0d7003a319f41c0da0b0f2b447
Rob Allen <rob@akrabat.com>
2023-04-02 21:18:13 +0100

    Update middleware.php

--- config/middleware.php
+++ config/middleware.php
@@ -10,13 +10,14 @@ return static function (App $app, Env $env) {
     /** @var Psr\Container\ContainerInterface $container */
     $container = $app->getContainer();
 
+    // The last middleware layer added is the first to be executed.
+    // This means that bottom of this list runs first (is on the outside) and the one at the top, runs last as it
+    // is the inner most one.
+    $app->addBodyParsingMiddleware();
     $app->addErrorMiddleware(
         displayErrorDetails: $env->getBool('DISPLAY_ERROR_DETAILS'),
         logErrors: $env->getBool('LOG_ERRORS', true),
         logErrorDetails: true,
         logger: $container->get(LoggerInterface::class),
     );
-
-    $app->addBodyParsingMiddleware();
-    $app->addRoutingMiddleware();
 };
